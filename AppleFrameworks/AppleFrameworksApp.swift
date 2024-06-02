72194d46b5b020d9541fa4e62687ad11db749a9b
Zain Ashraf <ashraf.zain.a@gmail.com>
2024-06-01 23:53:37 -0400

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Zain Ashraf on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
